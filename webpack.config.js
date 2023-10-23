4e427120924ef1005ce062b35ec4cd07fb0a4ac1
Денис <rediskink@inbox.ru>
2023-10-23 14:43:55 +0700

    commit with some fixes

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './build'),
     filename: "bundle.js",
     assetModuleFilename: 'assets/[hash][ext][query]',
     clean: true,
